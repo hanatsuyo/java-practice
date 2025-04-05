6f19f0522a58d61368b90fe450f487a3a66cd91e
hanatsuyo <hanatsuyo.t5.14@gmail.com>
2025-04-05 23:58:43 +0900

    オートボクシングの練習

---
+++ src/com/example/type/AutoBoxing.java
@@ -0,0 +1,11 @@
+package com.example.type;
+
+public class AutoBoxing {
+    public static void main(String[] args) {
+        int num = 10;
+        Integer numInt = 10;
+        Integer sum = num + numInt;
+        System.out.println(sum);
+
+    }
+}
