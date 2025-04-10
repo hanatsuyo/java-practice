1a97c587226ec4f88cdd93ef3de13bd39de3675e
hanatsuyo <hanatsuyo.t5.14@gmail.com>
2025-04-11 00:31:13 +0900

    enumに値を持たせる練習

---
+++ src/com/example/enumsample/MainValue.java
@@ -0,0 +1,10 @@
+package com.example.enumsample;
+
+public class MainValue {
+    
+    public static void main(String[] args) {
+        HttpStatus hs = HttpStatus.OK;
+        System.out.println(hs);
+        System.out.println(hs.getValue());
+    }
+}
