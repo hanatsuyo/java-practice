8ee62908e421327ecd1887b931007e0b5bea4864
hanatsuyo <hanatsuyo.t5.14@gmail.com>
2025-04-12 00:21:00 +0900

    ソートを実装

--- src/com/example/arraysample/MainSample.java
+++ src/com/example/arraysample/MainSample.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class MainSample {
     public static void main(String... args) {
         int[] array1 = new int[10];
@@ -6,5 +8,11 @@ public class MainSample {
         System.out.println(array1.length);
         System.out.println(array2.length);
         System.out.println(array3.length);
+
+        int[] array4 = { 1, 2, 14, 3, 4, 0};
+        System.out.println(array4);
+        System.out.println(Arrays.toString(array4));
+        Arrays.sort(array4);
+        System.out.println(Arrays.toString(array4));
     }
 }
