d7fbe20d81bf26702b92e572181431df038811d0
hanatsuyo <hanatsuyo.t5.14@gmail.com>
2025-04-12 00:03:48 +0900

    配列の基礎

---
+++ src/com/example/arraysample/MainSample.java
@@ -0,0 +1,10 @@
+public class MainSample {
+    public static void main(String... args) {
+        int[] array1 = new int[10];
+        int[] array2 = { 1, 2, 3 };
+        int[] array3 = new int[] { 1, 2, 3 };
+        System.out.println(array1.length);
+        System.out.println(array2.length);
+        System.out.println(array3.length);
+    }
+}
